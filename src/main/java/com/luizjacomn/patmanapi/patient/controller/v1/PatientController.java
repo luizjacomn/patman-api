6daf3ce948f70404aa265dd8b4640fb20cb36781
Luiz Jacó <luizjaco.dev@gmail.com>
2026-07-11 17:30:19 -0300

    feat: Deleting patient

--- src/main/java/com/luizjacomn/patmanapi/patient/controller/v1/PatientController.java
+++ src/main/java/com/luizjacomn/patmanapi/patient/controller/v1/PatientController.java
@@ -13,6 +13,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,12 @@ public class PatientController {
     @GetMapping("/{id}")
     public PatientDetailResponse find(@PathVariable UUID id) {
         return patientDetailResponseMapper.to(patientService.buscarPorId(id));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable UUID id) {
+        patientService.deletar(id);
     }
 
 }
