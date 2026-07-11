6a1d8c722c2fd671bd80ddb9a134441c07ffeb9b
Luiz Jacó <luizjaco.dev@gmail.com>
2026-07-11 12:21:00 -0300

    improve: Criando teste para health check da API (actuator)

--- src/test/java/com/luizjacomn/patmanapi/PatmanApiApplicationTests.java
+++ src/test/java/com/luizjacomn/patmanapi/PatmanApiApplicationTests.java
@@ -1,14 +1,41 @@
 package com.luizjacomn.patmanapi;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class PatmanApiApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SneakyThrows
 	@Test
-	void contextLoads() { }
+	void shouldReturnHealthStatusUp() {
+        // arrange
+        RequestBuilder request = MockMvcRequestBuilders.get("/actuator/health")
+            .accept(MediaType.APPLICATION_JSON);
+
+        // act
+        ResultActions resultActions = mockMvc.perform(request);
+
+        // assert
+        resultActions.andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("UP"));
+    }
 
 }
